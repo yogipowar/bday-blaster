0b39f2c5015766c505a743af87282982cb297698
Yogesh Powar <yogesh.onpoint@gmail.com>
2025-05-17 22:24:04 +0530

    completev floww

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import App from './App.tsx';
 import './index.css';
 import React from 'react';
 
